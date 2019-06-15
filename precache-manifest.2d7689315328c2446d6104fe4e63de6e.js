ac9d91f9e31edb2a5da8cb457ac54bdbd6c67db2
TomislavBatar <wmbl.info@gmail.com>
2019-06-15 17:16:08 +0200

    Updates

---
+++ precache-manifest.2d7689315328c2446d6104fe4e63de6e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9998b9b235c5a5090805bea62cdf9c1",
+    "url": "/sezona/index.html"
+  },
+  {
+    "revision": "86f51da83c318c3ca1bb",
+    "url": "/sezona/static/css/2.d16ff606.chunk.css"
+  },
+  {
+    "revision": "b12b2c9fe72353e16748",
+    "url": "/sezona/static/css/main.a3fdcbb3.chunk.css"
+  },
+  {
+    "revision": "86f51da83c318c3ca1bb",
+    "url": "/sezona/static/js/2.c5effdb3.chunk.js"
+  },
+  {
+    "revision": "b12b2c9fe72353e16748",
+    "url": "/sezona/static/js/main.92a259f7.chunk.js"
+  },
+  {
+    "revision": "d15807dd1474aa8e12b4",
+    "url": "/sezona/static/js/runtime~main.60e9b267.js"
+  },
+  {
+    "revision": "7debc97a3ffa0794d80948b29e8d57f0",
+    "url": "/sezona/static/media/GrafikUsluge.7debc97a.png"
+  },
+  {
+    "revision": "deb4e7b8eb35a54398b81d1bf4219b6f",
+    "url": "/sezona/static/media/Kasko-osiguranje.deb4e7b8.png"
+  },
+  {
+    "revision": "844960dab979c0147c3a76b1576e2d41",
+    "url": "/sezona/static/media/Osiguranje-korisnika-kredita.844960da.png"
+  },
+  {
+    "revision": "f7564d806537557b1c600e06e277bac1",
+    "url": "/sezona/static/media/Putno-zdravstveno-osiguranje.f7564d80.png"
+  },
+  {
+    "revision": "4bcf9b56ac63ad884eee3397a0ff9e8b",
+    "url": "/sezona/static/media/arrow-right.4bcf9b56.svg"
+  },
+  {
+    "revision": "f1fe09b8a41a223aacc4b41ca4c6a504",
+    "url": "/sezona/static/media/aura-logo-web.f1fe09b8.svg"
+  },
+  {
+    "revision": "603d2a15601c385aaccfc09ec407dc50",
+    "url": "/sezona/static/media/automobili.603d2a15.png"
+  },
+  {
+    "revision": "21fc980d99390a5a0606e5f392940148",
+    "url": "/sezona/static/media/close.21fc980d.svg"
+  },
+  {
+    "revision": "8636de957977dedc67d4472a12cb9084",
+    "url": "/sezona/static/media/fb.8636de95.svg"
+  },
+  {
+    "revision": "cc5e92b0767b99437ee32d191c9c2342",
+    "url": "/sezona/static/media/ig.cc5e92b0.svg"
+  },
+  {
+    "revision": "6bcb402bbc2f684fd7fdee3a975f07fc",
+    "url": "/sezona/static/media/imovina.6bcb402b.png"
+  },
+  {
+    "revision": "6ffd713364ae4adc17131d803f7a7428",
+    "url": "/sezona/static/media/konstantan-rast.6ffd7133.png"
+  },
+  {
+    "revision": "3077ff468fc213a6c681545c8d7393ed",
+    "url": "/sezona/static/media/logo-aura-osiguranje.3077ff46.svg"
+  },
+  {
+    "revision": "0b43d0a6985a2ca0d2641ec2ff8e5081",
+    "url": "/sezona/static/media/mapa-bih.0b43d0a6.png"
+  },
+  {
+    "revision": "b2935c0ca1a8b9cad375ac60f892306b",
+    "url": "/sezona/static/media/newsletter.b2935c0c.png"
+  },
+  {
+    "revision": "a1cd80651e2dd63eb80f0c28fcca4e76",
+    "url": "/sezona/static/media/open.a1cd8065.svg"
+  },
+  {
+    "revision": "930b2b83ba448fb6bb77aa2e6c789f17",
+    "url": "/sezona/static/media/ostalo.930b2b83.png"
+  },
+  {
+    "revision": "76be43d1bc54b32aa1e73745e0e0e72d",
+    "url": "/sezona/static/media/putovanja.76be43d1.png"
+  },
+  {
+    "revision": "2d77448dd9c2a5db058cad6a5e5d0f67",
+    "url": "/sezona/static/media/search.2d77448d.svg"
+  },
+  {
+    "revision": "bfea3f497c3d9434b49b75c9414a7bc5",
+    "url": "/sezona/static/media/search.bfea3f49.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/sezona/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/sezona/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/sezona/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/sezona/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "853a1cd71bca19423c90cf1c2d732feb",
+    "url": "/sezona/static/media/strucan-tim.853a1cd7.png"
+  },
+  {
+    "revision": "2170135e5fe6a726eb4d7215b94cc628",
+    "url": "/sezona/static/media/unapredjivanje-usluga.2170135e.png"
+  },
+  {
+    "revision": "708888ea2a31c971ac936391ada93f07",
+    "url": "/sezona/static/media/viber.708888ea.svg"
+  },
+  {
+    "revision": "00c96fb0ec7b51cbc017aded11fab33f",
+    "url": "/sezona/static/media/yt.00c96fb0.svg"
+  },
+  {
+    "revision": "9fcb8168b208722f6fc369e902ed3041",
+    "url": "/sezona/static/media/zadovoljni-klijenti.9fcb8168.png"
+  }
+]);
